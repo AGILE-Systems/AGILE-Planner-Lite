c52607e515509c9b7b75e7443bb8caf0eaad63b6
AndrewRoe34 <andrewroe34@yahoo.com>
2021-08-03 20:33:53 -0400

    [Implement] Update to Time

---
+++ AGILE Planner Lite/test/agile/planner/manager/day/DayTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package agile.planner.manager.day;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Andrew Roe
+ *
+ */
+public class DayTest {
+
+	/**
+	 * Test method for {@link agile.planner.manager.day.Day#getDate()}.
+	 */
+	@Test
+	public void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link agile.planner.manager.day.Day#addSubTask(agile.planner.task.Task)}.
+	 */
+	@Test
+	public void testAddSubTask() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link agile.planner.manager.day.Day#getSpareHours()}.
+	 */
+	@Test
+	public void testGetSpareHours() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link agile.planner.manager.day.Day#hasSpareHours()}.
+	 */
+	@Test
+	public void testHasSpareHours() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link agile.planner.manager.day.Day#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AGILE Planner Lite/test/agile/planner/util/TimeTest.java
@@ -0,0 +1,30 @@
+package agile.planner.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests Time functionality
+ * 
+ * @author Andrew Roe
+ */
+public class TimeTest {
+
+	/**
+	 * Tests determineRangeOfDays() functionality
+	 */
+	@Test
+	public void testDetermineRangeOfDays() {
+		
+	}
+
+	/**
+	 * Tests getFormattedCalendarInstance() functionality
+	 */
+	@Test
+	public void testGetFormattedCalendarInstance() {
+		
+	}
+
+}
